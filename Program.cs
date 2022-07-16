cadd12301b3c7326df08264f594ca6021e76245d
Yannick Robert <43834308+yjmrobert@users.noreply.github.com>
2022-07-16 18:17:03 +0000

    added mud

--- Program.cs
+++ Program.cs
@@ -1,10 +1,13 @@
+using fever;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using fever;
+using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddMudServices();
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
